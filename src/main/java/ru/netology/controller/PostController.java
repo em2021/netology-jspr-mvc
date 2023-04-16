7a089e2764b0a72212b0c26ba3a026bcd44122d2
em2021 <egor_m@icloud.com>
2023-04-17 01:29:10 +0300

    Migrated to Tomcat Embed and Spring MVC

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,16 +1,15 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
-import ru.netology.exception.NotFoundException;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Reader;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-    public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
     private final Gson gson = new Gson();
 
@@ -18,37 +17,23 @@ public class PostController {
         this.service = service;
     }
 
-    public void all(HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var data = service.all();
-        response.getWriter().print(gson.toJson(data));
+    @GetMapping
+    public List<Post> all() {
+        return service.all();
     }
 
-    public void getById(long id, HttpServletResponse response) throws IOException {
-        try {
-            final var data = service.getById(id);
-            response.getWriter().print(gson.toJson(data));
-        } catch (NotFoundException e) {
-            response.setStatus(204);
-        }
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
     }
 
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var post = gson.fromJson(body, Post.class);
-        final var data = service.save(post);
-        response.setStatus(201);
-        response.getWriter().print(gson.toJson(data));
+    @PostMapping
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
     }
 
-    public void removeById(long id, HttpServletResponse response) throws IOException {
-        try {
-            service.getById(id);
-        } catch (NotFoundException e) {
-            response.setStatus(204);
-            return;
-        }
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id) {
         service.removeById(id);
-        response.getWriter().print("Post with id " + id + " removed successfully");
     }
 }
